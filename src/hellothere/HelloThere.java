ebc4f58e2836be249038db84b5c79a0121dd47a4
Alex Hersman <Alexh0828@gmail.com>
2019-08-26 16:44:56 -0500

    git commit -a

--- src/hellothere/HelloThere.java
+++ src/hellothere/HelloThere.java
@@ -7,6 +7,7 @@ public class HelloThere {
     
     public static void main(String[] args) {
         System.out.println("Hello there.");
+        System.out.println("General Kenobi!");
     }
     
 }
